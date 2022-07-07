3a8e208dd28f6913fed715544ced16cdf6d424d9
Ugly_Potato <71345965+hyoseo837@users.noreply.github.com>
2022-07-07 23:13:31 +0200

    Day 16

--- rubik.py
+++ rubik.py
@@ -9,8 +9,19 @@ class cube:
             self.edge.append(string_edge[i])
     
     def prt(self):
-        for i in self.vertex:
-            print(i)
+        print(f"""
+        {self.vertex[0]}---{self.edge[0]}---{self.vertex[1]}     
+          {self.edge[3]}       {self.edge[1]}
+            {self.vertex[2]}---{self.edge[2]}---{self.vertex[3]}
+
+        {self.edge[4]}--------{self.edge[5]}
+            {self.edge[7]}-------{self.edge[6]}
+
+        {self.vertex[4]}---{self.edge[8]}---{self.vertex[5]}     
+          {self.edge[11]}       {self.edge[9]}
+            {self.vertex[7]}---{self.edge[10]}---{self.vertex[6]}  
+        
+        """)
 
     def move(self,code):
         if code == "U":
@@ -19,12 +30,24 @@ class cube:
             self.vertex[3] = self.vertex[2]
             self.vertex[2] = self.vertex[1]
             self.vertex[1] = tmp
+            
+            tmp = self.edge[0]
+            self.edge[0] = self.edge[3]
+            self.edge[3] = self.edge[2]
+            self.edge[2] = self.edge[1]
+            self.edge[1] = tmp
         if code == "u":
             tmp = self.vertex[0]
             self.vertex[0] = self.vertex[1]
             self.vertex[1] = self.vertex[2]
             self.vertex[2] = self.vertex[3]
             self.vertex[3] = tmp
+            
+            tmp = self.edge[0]
+            self.edge[0] = self.edge[1]
+            self.edge[1] = self.edge[2]
+            self.edge[2] = self.edge[3]
+            self.edge[3] = tmp
 
         if code == "D":
             tmp = self.vertex[4]
@@ -32,6 +55,12 @@ class cube:
             self.vertex[5] = self.vertex[6]
             self.vertex[6] = self.vertex[7]
             self.vertex[7] = tmp
+
+            tmp = self.edge[8]
+            self.edge[8] = self.edge[9]
+            self.edge[9] = self.edge[10]
+            self.edge[10] = self.edge[11]
+            self.edge[11] = tmp
         if code == "d":
             tmp = self.vertex[4]
             self.vertex[4] = self.vertex[7]
@@ -39,12 +68,24 @@ class cube:
             self.vertex[6] = self.vertex[5]
             self.vertex[5] = tmp
 
+            tmp = self.edge[8]
+            self.edge[8] = self.edge[11]
+            self.edge[11] = self.edge[10]
+            self.edge[10] = self.edge[9]
+            self.edge[9] = tmp
+
         if code == "R":
             tmp = self.vertex[1]
             self.vertex[1] = self.vertex[2]
             self.vertex[2] = self.vertex[6]
             self.vertex[6] = self.vertex[5]
             self.vertex[5] = tmp
+            
+            tmp = self.edge[1]
+            self.edge[1] = self.edge[6]
+            self.edge[6] = self.edge[9]
+            self.edge[9] = self.edge[5]
+            self.edge[5] = tmp
         if code == "r":
             tmp = self.vertex[1]
             self.vertex[1] = self.vertex[5]
@@ -52,18 +93,36 @@ class cube:
             self.vertex[6] = self.vertex[2]
             self.vertex[2] = tmp
             
+            tmp = self.edge[1]
+            self.edge[1] = self.edge[5]
+            self.edge[5] = self.edge[9]
+            self.edge[9] = self.edge[6]
+            self.edge[6] = tmp
+            
         if code == "L":
             tmp = self.vertex[0]
             self.vertex[0] = self.vertex[4]
             self.vertex[4] = self.vertex[7]
             self.vertex[7] = self.vertex[3]
             self.vertex[3] = tmp
+            
+            tmp = self.edge[3]
+            self.edge[3] = self.edge[4]
+            self.edge[4] = self.edge[11]
+            self.edge[11] = self.edge[7]
+            self.edge[7] = tmp
         if code == "l":
             tmp = self.vertex[0]
             self.vertex[0] = self.vertex[3]
             self.vertex[3] = self.vertex[7]
             self.vertex[7] = self.vertex[4]
             self.vertex[4] = tmp
+            
+            tmp = self.edge[3]
+            self.edge[3] = self.edge[7]
+            self.edge[7] = self.edge[11]
+            self.edge[11] = self.edge[4]
+            self.edge[4] = tmp
 
         if code == "F":
             tmp = self.vertex[7]
@@ -71,12 +130,24 @@ class cube:
             self.vertex[6] = self.vertex[2]
             self.vertex[2] = self.vertex[3]
             self.vertex[3] = tmp
+            
+            tmp = self.edge[2]
+            self.edge[2] = self.edge[7]
+            self.edge[7] = self.edge[10]
+            self.edge[10] = self.edge[6]
+            self.edge[6] = tmp
         if code == "f":
             tmp = self.vertex[7]
             self.vertex[7] = self.vertex[3]
             self.vertex[3] = self.vertex[2]
             self.vertex[2] = self.vertex[6]
             self.vertex[6] = tmp
+
+            tmp = self.edge[2]
+            self.edge[2] = self.edge[6]
+            self.edge[6] = self.edge[10]
+            self.edge[10] = self.edge[7]
+            self.edge[7] = tmp
             
         if code == "B":
             tmp = self.vertex[0]
@@ -84,12 +155,24 @@ class cube:
             self.vertex[1] = self.vertex[5]
             self.vertex[5] = self.vertex[4]
             self.vertex[4] = tmp
+
+            tmp = self.edge[0]
+            self.edge[0] = self.edge[5]
+            self.edge[5] = self.edge[8]
+            self.edge[8] = self.edge[4]
+            self.edge[4] = tmp
         if code == "b":
             tmp = self.vertex[0]
             self.vertex[0] = self.vertex[4]
             self.vertex[4] = self.vertex[5]
             self.vertex[5] = self.vertex[1]
             self.vertex[1] = tmp
+            
+            tmp = self.edge[0]
+            self.edge[0] = self.edge[4]
+            self.edge[4] = self.edge[8]
+            self.edge[8] = self.edge[5]
+            self.edge[5] = tmp
 
 foo = cube("12345678","abcdefghijkl")
 foo.prt()
