5729532d192c6c43db5f8c12152225e836c3361e
Ugly_Potato <71345965+hyoseo837@users.noreply.github.com>
2022-07-15 23:16:14 +0200

    day 21

--- ui.py
+++ ui.py
@@ -15,9 +15,11 @@ pygame.display.set_caption("cube solver")
 clock = pygame.time.Clock()
 
 aaFont = pygame.font.Font(None, 20)
+background = pygame.image.load("images/background.png")
 
-coordinates = [(100,100),(100,250),(150,200)]
-canvas = pygame.Surface((300,300))
+coordinates = [(275,50),(405,125),(405,275),(275,350),(145,275),(145,125)]
+shapes = [[(405,125),(405,275),(275,350),(275,200)],[(275,350),(145,275),(145,125),(275,200)],[(145,125),(275,50),(405,125),(275,200)]]
+canvas = pygame.Surface((550,400))
 
 running = True
 while running:
@@ -36,8 +38,13 @@ while running:
     # screen.blit(background, (0, 0))
     
 
-    pygame.draw.polygon(canvas, (255,255,0), coordinates)
-    screen.blit(canvas, (0,0))
+    # pygame.draw.polygon(canvas, (160,160,160), coordinates)
+    pygame.draw.polygon(canvas, (255,0,0), shapes[0])
+    pygame.draw.polygon(canvas, (0,255,0), shapes[1])
+    pygame.draw.polygon(canvas, (0,0,255), shapes[2])
+
+    screen.blit(background,(0,0))
+    screen.blit(canvas, (50,75))
     screen.blit(text,(30,30))
 
     # Update Screen
